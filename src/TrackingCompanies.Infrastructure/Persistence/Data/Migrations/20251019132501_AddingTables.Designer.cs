25fc6adc548262335f142de96311e0a1a2467a5c
Emmanuel Gomes <aeag@outlook.com>
2025-10-19 14:37:33 +0100

    Monitoring

--- src/TrackingCompanies.Infrastructure/Persistence/Data/Migrations/20251019132501_AddingTables.Designer.cs
+++ src/TrackingCompanies.Infrastructure/Persistence/Data/Migrations/20251019132501_AddingTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TrackingCompanies.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using TrackingCompanies.Infrastructure.Persistence;
 namespace TrackingCompanies.Infrastructure.Persistence.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251019132501_AddingTables")]
+    partial class AddingTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
